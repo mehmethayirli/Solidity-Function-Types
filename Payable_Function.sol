7311e6746ea7d97bcc4f5cc35a38c79d3768b9a8
Mehmet <98388377+mehmethayirli@users.noreply.github.com>
2023-04-07 20:05:46 +0300

    Add files via upload

---
+++ Payable_Function.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+contract cuberium {
+    address private owner;
+
+    constructor() {
+        owner= msg.sender;
+    }
+
+    function paraGonder() public payable {
+        require(msg.value > 0, "Bir miktar Ether gondermelisiniz.");
+        require(msg.sender != owner, "owner bu fonksiyonu cagiramaz.");
+        // Alınan Ether ile bir işlem yapabilirsiniz
+    }
+}
